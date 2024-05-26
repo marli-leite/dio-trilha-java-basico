e15830d4425927f8c61131390fb7fc342db5fc07
marli-leite <marli.leite2301@outlook.com>
2024-05-25 23:01:02 -0300

    Alterações estudos trilha java

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,7 +1,24 @@
 public class TipoVariaveis {
       
       public static void main(String [] args) throws Exception {
-            System.out.println("Hello,World");
+     /*   ESTUDAR TIPOS PRIMITIVOS 
+      A CLASSE STRING QUE REPRESENTA TEXTO NA APLICAÇÃO
+      EXEMPLO ABAIXO*/
+      
+     
+           
+           
+           /* double salarioMinimo = 2500;*/ 
+           /*  short numeroCurto = 1;*/
+           /* int numeroNormal = numeroCurto;*/ 
+           /* short numeroclearCurto2 = (short) numeroNormal;*/ 
+            int numero = 5;
+
+            numero = 10;
+      System.out.println(numero);
+/*Para determinar que um numero nunca será modificado temos que 
+declarar que seja final no inicio do comando e por convenção as constantes 
+são sempre escrtitas em CAIXA ALTA */
+      }
 
       }
-}
